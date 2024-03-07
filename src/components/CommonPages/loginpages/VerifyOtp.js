995a83df7e969064e5ff75587994baee5fd74baa
developer <developer.phanomprofessionals@gmail.com>
2024-03-07 11:33:08 +0530

    as

--- src/components/CommonPages/loginpages/VerifyOtp.js
+++ src/components/CommonPages/loginpages/VerifyOtp.js
@@ -13,7 +13,7 @@ export default function VerifyOtp({ onVerification }) {
   const userDetails = useSelector((state) => state?.users.user);
 
   const [otpintial, setChangePass] = useState({
-    email: "avineetsingh78@gmail.com",
+    email: "karan.sharma111@yahoo.co.in",
   });
   const handleChange = (e) => {
     setOtp(e.target.value);
